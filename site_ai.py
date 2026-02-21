94b80dda7ef051981343ff6b9639efeb2c54a5e2
i70355614-creator <i70355614@gmail.com>
2026-02-21 23:21:50 +0200

    Update site_ai.py

--- site_ai.py
+++ site_ai.py
@@ -10,9 +10,10 @@ intrebare = st.chat_input("Scrie ceva aici....")
 
 if intrebare:
 
-    model=genai.GenerativeModel("models/gemini-pro-latest")
+    model=genai.GenerativeModel("models/gemini-1.5-flash")
     try:
         response = model.generate_content(intrebare)
         st.write(response.text)
     except Exception as e:
+
         st.error(f"A aparut o problema: {e}")
