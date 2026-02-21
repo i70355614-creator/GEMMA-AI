6c3f5e99361fa7ec89cd85893af0044131e93c4f
i70355614-creator <i70355614@gmail.com>
2026-02-21 23:06:27 +0200

    Add files via upload

---
+++ site_ai.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+
+genai.configure(api_key="AIzaSyBv3TMYQu1jICVVfn11zsbUKdVLqeGGLUM")
+
+st.title("GEMMA AI")
+
+intrebare = st.chat_input("Scrie ceva aici....")
+
+if intrebare:
+
+    model=genai.GenerativeModel("models/gemini-pro-latest")
+    try:
+        response = model.generate_content(intrebare)
+        st.write(response.text)
+    except Exception as e:
+        st.error(f"A aparut o problema: {e}")
